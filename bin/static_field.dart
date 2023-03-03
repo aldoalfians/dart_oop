72493d5119f7d0b192753158ea57403bdeb53d2b
aldoalfians <aldoboetd908@gmail.com>
2023-03-04 00:03:43 +0700

    static , equals , methotd to string

---
+++ bin/static_field.dart
@@ -0,0 +1,11 @@
+class Application {
+
+  static final String name = 'Belajar Dart OOP';
+  static final String author = 'Aldo Alfiansyah';
+
+}
+
+void main(){
+  print(Application.name);
+  print(Application.author);
+}
